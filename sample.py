31044a3d0f62cfe9641ca1b935c3a8d3edc2568e
Alfa0112 <syana.and.yuji@gmail.com>
2023-02-19 11:27:47 +0900

    sampleを最もシンプルな形に変更

--- sample.py
+++ sample.py
@@ -1,37 +1,28 @@
 import argparse
 import datetime
 from pathlib import Path
-import xml.etree.ElementTree as ET
-import zipfile
 
-import pandas as pd
-
-from .hyper_bloom import HyperBloomGame, HyperBloomDataXML, CardImageTable
+from pyudon import Game, Deck
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("images", type=Path)
-    parser.add_argument("card_image_table", type=Path,
-                        help="The csv file with correspondence between card names and images.")
     parser.add_argument("output_dir", type=Path)
-    parser.add_argument("--bg-img", type=Path,
-                        help="The image file to be set to background of the game.")
     args = parser.parse_args()
 
-    # カード定義読み込み
-    card_image_table_df = pd.read_csv(args.card_image_table)    
-    card_image_table = CardImageTable(args.images,
-                                    card_image_table_df)
-
     # ゲーム生成
-    hb_game = HyperBloomGame(card_image_table, args.bg_img)
-    hb_game.create_random_field()
+    game = Game()
+
+    # デッキ生成
+    deck = Deck()
+
+    # デッキ追加
+    # game.add_deck(deck)
 
     # Zipファイル作成
-    now = datetime.datetime.now()
-    zip_path = args.output_dir / now.strftime("hb_%y%m%d_%H%M%S.zip")
-    hb_game.create_zip(zip_path)
+    now = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+    zip_path = args.output_dir / f"{now}.zip"
+    game.create_zip(zip_path)
 
 
 if __name__ == "__main__":
