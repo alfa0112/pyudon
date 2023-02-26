608f31ed550b325f1d49fcd32c653315218a9342
Alfa0112 <syana.and.yuji@gmail.com>
2023-02-27 00:53:21 +0900

    ゲームオブジェクトの各クラスを追加

--- pyudon/game.py
+++ pyudon/game.py
@@ -1,153 +1,141 @@
-import random
 import zipfile
+from dataclasses import dataclass, field
+from enum import Enum, auto
 from pathlib import Path
-
-from pandas import DataFrame
 
 from .chat_xml import ChatXML
 from .data_xml import DataXML
 from .summary_xml import SummaryXML
-from .util import HashMaker
+from .util import HashMaker, Image
+
+
+class TableGridType(Enum):
+    SQUARE = auto()
+    HEX_VERTICAL = auto()
+    HEX_HORIZONTAL = auto()
+
+
+class TableGridColor(Enum):
+    BLACK = auto()
+    WHITE = auto()
+
+
+class TableBackgroundFilter(Enum):
+    NONE = auto()
+    BLACK = auto()
+    WHITE = auto()
+
+
+@dataclass(frozen=True)
+class Table:
+    name: str
+    image: Image
+    width: float = 20
+    height: float = 20
+    background_image: Image = None
+    grid_type: TableGridType = TableGridType.SQUARE
+    grid_color: TableGridColor = TableGridColor.BLACK
+    background_filter: TableBackgroundFilter = TableBackgroundFilter.NONE
+    show_grid: bool = False
+
+
+@dataclass(frozen=True)
+class CharacterResource:
+    name: str
+    initial_value: float
+    max_value: float
+
+
+@dataclass(frozen=True)
+class Character:
+    name: str
+    size: float = 1
+    resources: list[CharacterResource] = field(default_factory=list)
+    image: Image = None
+    info: str = None
+
+
+@dataclass(frozen=True)
+class Card:
+    name: str
+    front_image: Image
+    back_image: Image
+
+
+class Deck:
+    name: str
+    card_list: list[Card]
+
+    def add_card(self, card: Card) -> None:
+        pass
+
+    def add_cards(self, cards: list[Card]) -> None:
+        pass
+
+
+class DiceType(Enum):
+    SIDED_4 = auto()
+    SIDED_6 = auto()
+    SIDED_8 = auto()
+    SIDED_10 = auto()
+    SIDED_10_PLACES_10 = auto()
+    SIDED_12 = auto()
+    SIDED_20 = auto()
+
+
+@dataclass(frozen=True)
+class Dice:
+    name: str
+    type: DiceType
 
 
 class Game:
-    def __init__(self):
+    def __init__(self, table: Table) -> None:
         self._data_xml = DataXML()
         self._chat_xml = ChatXML()
         self._summary_xml = SummaryXML()
 
         self._hash_maker = HashMaker()
 
-    def create_zip(self, file_path: Path):
+        self._tables: list[Table] = [table]
+
+    def create_zip(self, file_path: Path) -> None:
+        # data.xml組み立て
+        for table in self._tables:
+            self._data_xml.add_game_table(
+                table.name,
+                table.width,
+                table.height,
+                image_identifier=table.image.hashnized_name
+            )
+
+        # ファイル書き込み
         with (file_path.open("bw") as f,
               zipfile.ZipFile(f, "w") as zipf):
             zipf.writestr("data.xml", self._data_xml.get_body())
             zipf.writestr("chat.xml", self._chat_xml.get_body())
             zipf.writestr("summary.xml", self._summary_xml.get_body())
 
+            self._dump_all_images(zipf)
 
-class Deck:
-    pass
+    def _dump_all_images(self, zipf: zipfile.ZipFile) -> None:
+        for table in self._tables:
+            table.image.to_zipfile(zipf)
 
+    def add_table(self, table: Table) -> None:
+        self._tables.append(table)
 
-class CardImageTable():
-    def __init__(self, img_dir: Path, card_image_table_df: DataFrame):
-        self._card_image_table_df = card_image_table_df.copy()
-        self._card_image_table_df["image"] = self._card_image_table_df["image"].map(lambda name: img_dir / name)
+    def add_character(self, character: Character, x: float, y: float) -> None:
+        pass
 
-        self._dict = {series["name"]: series["image"]
-                        for _, series
-                        in self._card_image_table_df.iterrows()}
+    def add_card(self, card: Card, x: float, y: float) -> None:
+        pass
 
-    def get_dict(self) -> dict:
-        return self._dict
+    def add_deck(self, deck: Deck, x: float, y: float) -> None:
+        pass
 
-    def iter_img_paths(self):
-        for _, series in self._card_image_table_df.iterrows():
-            yield series["image"]
+    def add_dice(self, dice: Dice, x: float, y: float) -> None:
+        pass
 
-
-class Card():
-    def __init__(self, name, front, back):
-        self._name = name
-        self._front = front
-        self._back = back
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def front(self):
-        return self._front
-
-    @property
-    def back(self):
-        return self._back
-
-
-class ClockWorkerDataXML():
-    def __init__(self,
-                cards_image_dict,
-                table_image_path=None):
-        self._data_xml = DataXML()
-        self._data_xml.first_table_node.set_height(0)
-        self._data_xml.first_table_node.set_width(0)
-        if table_image_path:
-            self._data_xml.first_table_node.set_bg_img_from_path(table_image_path)
-
-        player_1 = self._data_xml.add_character("Player1", size=2, x=-450, y=-200)
-        player_1.add_resource("VP", 100, 0)
-
-        self._cards_image_dict = cards_image_dict
-
-        self._magical_sercle_deck_lv1_red = self._data_xml.add_card_stack("MagicalSercleCardDeckLv1Red",
-                                                                -300, -200)
-        self._magical_sercle_deck_lv1_red.add_card_from_path(self._cards_image_dict["sercle1red"],
-                                                            self._cards_image_dict["back"],
-                                                            5)
-        self._magical_sercle_deck_lv2_red = self._data_xml.add_card_stack("MagicalSercleCardDeckLv2Red",
-                                                                -200, -200)
-        self._magical_sercle_deck_lv2_red.add_card_from_path(self._cards_image_dict["sercle2red"],
-                                                            self._cards_image_dict["back"],
-                                                            5)
-        self._magical_sercle_deck_lv3_red = self._data_xml.add_card_stack("MagicalSercleCardDeckLv2Red",
-                                                                -100, -200)
-        self._magical_sercle_deck_lv3_red.add_card_from_path(self._cards_image_dict["sercle3red"],
-                                                            self._cards_image_dict["back"],
-                                                            3)
-
-        self._magical_sercle_deck_lv1_blue = self._data_xml.add_card_stack("MagicalSercleCardDeckLv1Blue",
-                                                                -300, -50)
-        self._magical_sercle_deck_lv1_blue.add_card_from_path(self._cards_image_dict["sercle1blue"],
-                                                            self._cards_image_dict["back"],
-                                                            5)
-        self._magical_sercle_deck_lv2_blue = self._data_xml.add_card_stack("MagicalSercleCardDeckLv2Blue",
-                                                                -200, -50)
-        self._magical_sercle_deck_lv2_blue.add_card_from_path(self._cards_image_dict["sercle2blue"],
-                                                            self._cards_image_dict["back"],
-                                                            5)
-        self._magical_sercle_deck_lv3_blue = self._data_xml.add_card_stack("MagicalSercleCardDeckLv3Blue",
-                                                                -100, -50)
-        self._magical_sercle_deck_lv3_blue.add_card_from_path(self._cards_image_dict["sercle3blue"],
-                                                            self._cards_image_dict["back"],
-                                                            3)
-
-    def add_card_to_board(self, front_path, back_path, x, y, state=0):
-        self._data_xml.add_card_from_path(front_path, back_path, x, y, state=state)
-
-    def create_random_field(self, x=0, y=0, x_distance=100, y_distance=130):
-        cards = []
-        cards.extend([ClockWorkerCard("rock",
-                                    self._cards_image_dict["rock"],
-                                    self._cards_image_dict["back"])] * 2)
-        cards.extend([ClockWorkerCard("1st",
-                                    self._cards_image_dict["1st"],
-                                    self._cards_image_dict["2pt"])] * 3)
-        cards.extend([ClockWorkerCard("2nd",
-                                    self._cards_image_dict["2nd"],
-                                    self._cards_image_dict["1pt"])] * 10)
-        cards.extend([ClockWorkerCard("3rd",
-                                    self._cards_image_dict["3rd"],
-                                    self._cards_image_dict["2pt"])] * 21)
-
-        random.shuffle(cards)
-
-        x = 0
-        y = 0
-        for card in cards:
-            if x > x_distance * 5:
-                x = 0
-                y += y_distance
-            self.add_card_to_board(card.front,
-                                    card.back,
-                                    x,
-                                    y,
-                                    state=0)
-            x += x_distance
-
-    def write(self, path, encoding="utf-8"):
-        self._data_xml.write(path, encoding)
-
-    def get_body(self, encoding="utf-8") -> str:
-        return self._data_xml.get_body(encoding)
+    def set_backgroud_image(self) -> None:
+        pass
