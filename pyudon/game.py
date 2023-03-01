a015185ef2ba00864c7c3aca72ae8392f4ea93ec
Alfa0112 <syana.and.yuji@gmail.com>
2023-03-02 00:16:38 +0900

    game.pyに各種クラスを追加。zipの生成処理を追記

--- pyudon/game.py
+++ pyudon/game.py
@@ -42,35 +42,85 @@ class Table:
 @dataclass(frozen=True)
 class CharacterResource:
     name: str
-    initial_value: float
+    curret_value: float
     max_value: float
 
 
 @dataclass(frozen=True)
+class CharacterNote:
+    name: str
+    value: str
+
+
+@dataclass
+class CharacterDetailSection:
+    name: str
+    notes: list[CharacterNote] = field(default_factory=list)
+    resources: list[CharacterResource] = field(default_factory=list)
+
+    def add_note(self, item: CharacterNote):
+        self.notes.append(item)
+
+    def add_resource(self, item: CharacterResource):
+        self.resources.append(item)
+
+
+@dataclass
 class Character:
     name: str
     size: float = 1
-    resources: list[CharacterResource] = field(default_factory=list)
     image: Image = None
-    info: str = None
+    detail_sections: list[CharacterDetailSection] = field(default_factory=list)
+
+    def add_detail_section(self, detail_section: CharacterDetailSection) -> None:
+        self.detail_sections.append(detail_section)
+
+
+@dataclass(frozen=True)
+class OnBoardCharacter:
+    character: Character
+    x: float
+    y: float
+
+
+class CardState(Enum):
+    TOP = 0
+    BOTTOM = 1
 
 
 @dataclass(frozen=True)
 class Card:
     name: str
-    front_image: Image
-    back_image: Image
+    top_image: Image
+    bottom_image: Image
+    size: float = 2.0
+    state: CardState = CardState.TOP
 
 
+@dataclass(frozen=True)
+class OnBoardCard:
+    card: Card
+    x: float
+    y: float
+
+
+@dataclass
 class Deck:
     name: str
-    card_list: list[Card]
+    cards: list[Card] = field(default_factory=list)
 
     def add_card(self, card: Card) -> None:
-        pass
+        self.cards.append(card)
 
     def add_cards(self, cards: list[Card]) -> None:
-        pass
+        self.cards.extend(cards)
+
+
+@dataclass
+class OnBoardDeck:
+    deck: Deck
+    x: float
+    y: float
 
 
 class DiceType(Enum):
@@ -98,6 +148,9 @@ class Game:
         self._hash_maker = HashMaker()
 
         self._tables: list[Table] = [table]
+        self._on_board_characters: list[OnBoardCharacter] = []
+        self._on_board_cards: list[OnBoardCard] = []
+        self._on_board_decks: list[OnBoardDeck] = []
 
     def create_zip(self, file_path: Path) -> None:
         # data.xml組み立て
@@ -108,6 +161,50 @@ class Game:
                 table.height,
                 image_identifier=table.image.hashnized_name
             )
+        for on_board_character in self._on_board_characters:
+            character = on_board_character.character
+            character_node = self._data_xml.add_character(
+                character.name,
+                character.size,
+                x=on_board_character.x,
+                y=on_board_character.y,
+                img_identifier=character.image.hashnized_name
+            )
+            for detail_section in character.detail_sections:
+                detail_section_node = character_node.add_detail_section(detail_section.name)
+                for resource in detail_section.resources:
+                    detail_section_node.add_resource(
+                        resource.name,
+                        resource.curret_value,
+                        resource.max_value
+                    )
+                for note in detail_section.notes:
+                    detail_section_node.add_note(
+                        note.name,
+                        note.value
+                    )
+        for on_board_card in self._on_board_cards:
+            self._data_xml.add_card(
+                on_board_card.card.name,
+                on_board_card.card.top_image.hashnized_name,
+                on_board_card.card.bottom_image.hashnized_name,
+                on_board_card.x,
+                on_board_card.y,
+                on_board_card.card.size,
+                on_board_card.card.state.value
+            )
+        for on_board_deck in self._on_board_decks:
+            deck_node = self._data_xml.add_card_stack(
+                on_board_deck.deck.name,
+                on_board_deck.x,
+                on_board_deck.y
+            )
+            for card in on_board_deck.deck.cards:
+                deck_node.add_card(
+                    card.name,
+                    card.top_image.hashnized_name,
+                    card.bottom_image.hashnized_name
+                )
 
         # ファイル書き込み
         with (file_path.open("bw") as f,
@@ -121,18 +218,23 @@ class Game:
     def _dump_all_images(self, zipf: zipfile.ZipFile) -> None:
         for table in self._tables:
             table.image.to_zipfile(zipf)
+        for on_board_character in self._on_board_characters:
+            on_board_character.character.image.to_zipfile(zipf)
+        for on_board_card in self._on_board_cards:
+            on_board_card.card.top_image.to_zipfile(zipf)
+            on_board_card.card.bottom_image.to_zipfile(zipf)
 
     def add_table(self, table: Table) -> None:
         self._tables.append(table)
 
     def add_character(self, character: Character, x: float, y: float) -> None:
-        pass
+        self._on_board_characters.append(OnBoardCharacter(character, x, y))
 
     def add_card(self, card: Card, x: float, y: float) -> None:
-        pass
+        self._on_board_cards.append(OnBoardCard(card, x, y))
 
     def add_deck(self, deck: Deck, x: float, y: float) -> None:
-        pass
+        self._on_board_decks.append(OnBoardDeck(deck, x, y))
 
     def add_dice(self, dice: Dice, x: float, y: float) -> None:
         pass
