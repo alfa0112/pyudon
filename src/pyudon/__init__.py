c275436faef2a345b73d23fcbf4dc14624d8f25d
Alfa0112 <syana.and.yuji@gmail.com>
2023-03-04 02:47:33 +0900

    インストール用のモジュールを修正

--- src/pyudon/__init__.py
+++ src/pyudon/__init__.py
@@ -1,4 +1,8 @@
-from ._version import __version__
+"""
+BUG: setuptoolsのバグ？で__version__を他のファイルからインポートしようとするとサブモジュールのインポート処理で失敗する
+ひとまず__version__を__init__.py内で宣言することで回避
+"""
+# from ._version import __version__
 __version__ = "0.2.0"
 
 from .data import DefaultBackgroudImage, Image
@@ -6,3 +10,7 @@ from .game import (Card, CardState, Character, CharacterDetailSection,
                    CharacterNote, CharacterResource, Deck, Dice, DiceType,
                    Game, OnBoardCard, OnBoardCharacter, OnBoardDeck, Table,
                    TableBackgroundFilter, TableGridColor, TableGridType)
+
+__all__ = ["DefaultBackgroudImage", "Image", "Card", "CardState", "Character", "CharacterDetailSection",
+           "CharacterNote", "CharacterResource", "Deck", "Dice", "DiceType", "Game", "OnBoardCard", "OnBoardCharacter",
+           "OnBoardDeck", "Table", "TableBackgroundFilter", "TableGridColor", "TableGridType"]
